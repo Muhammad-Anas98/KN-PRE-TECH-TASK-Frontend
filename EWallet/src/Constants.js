4f70f292fdf7966838454784e717b99e469a40dd
Muhammad-Anas98 <82109859+Muhammad-Anas98@users.noreply.github.com>
2021-11-05 16:55:26 +0500

    KN PRE TECH TASK FRONTEND

---
+++ EWallet/src/Constants.js
@@ -0,0 +1,1 @@
+export const API_URL_WALLET = 'http://localhost:8999/v1/api/wallet';
---
+++ EWallet/src/views/dashboard/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+const Dashboard = () => {
+
+
+  return (
+    <div>
+    <h2>Welcome to Muhammad Anas's Pre-Tech Task </h2>
+
+    <h4>Functionalities completed:</h4>
+
+    <br></br>
+    <ul>
+    <li> Create a new Wallet</li>
+    <br></br>
+    <li> TopUp/ Withdraw from a wallet </li>
+    <br></br>
+    <li> View list of wallets and filters</li>
+    </ul>
+
+    <h4>Please got to wallet tab to perform all the tasks.</h4>
+    </div>
+    )
+  }
+
+  export default Dashboard
